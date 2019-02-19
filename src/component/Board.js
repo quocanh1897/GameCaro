c6f2eb6c2c65a6636a59c4a6c747d381ac25dedd
anhnq6 <anhnq6@vng.com.vn>
2019-02-19 18:08:25 +0700

    add emoji

--- src/component/Board.js
+++ src/component/Board.js
@@ -5,8 +5,10 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Badge from "react-bootstrap/Badge";
+import Modal from "react-bootstrap/Modal";
 import socketIOClient from "socket.io-client";
 import SocketContext from "../socket-context";
+import { Link } from "react-router-dom";
 
 let socket;
 export default class Board extends React.Component {
@@ -15,7 +17,7 @@ export default class Board extends React.Component {
     super(props);
     const {
       height,
-      width,
+      width
       // match: { params }
     } = this.props;
     this.state = {
@@ -33,16 +35,48 @@ export default class Board extends React.Component {
       boardData: this.initBoardData(height, width),
       endpoint: "localhost:4001",
       height: height,
-      width: width
+      width: width,
+      modalShow: false,
+      modalLoseShow: false,
+      modalEmoShow: false,
+      win: false,
+      lose: false,
+      emoji: ""
     };
     this.onClick = this.onClick.bind(this);
-    // this.allowClick = this.allowClick.bind(this);
+    this.allowClick = this.allowClick.bind(this);
+    this.handleEmoji = this.handleEmoji.bind(this);
 
     this.updateInput = this.updateInput.bind(this);
     this.handleSubmitName = this.handleSubmitName.bind(this);
   }
 
+  allowClick() {
+    if (this.state.win || this.state.lose) {
+      alert("The game is ended.");
+      return;
+    }
+  }
+
+  handleEmoji(e) {
+    // console.log(e.target.innerText);
+    let req = {
+      emoji: e.target.innerText,
+      room: this.state.room,
+      id: this.state.id,
+      oppID: this.state.oppID
+    };
+    socket.emit("emoji", req);
+    socket.on("emoji-from-server", data => {
+      console.log(data.emoji);
+      this.setState({ emoji: data.emoji , modalEmoShow: true});
+    });
+  }
+
   onClick() {
+    if (this.state.win || this.state.lose) {
+      return;
+    }
     if (!this.state.nameOK) {
       alert("You must choose your name first!");
       return;
@@ -85,16 +119,16 @@ export default class Board extends React.Component {
             updatedBoard[i][j].isMyTurn = data.isMyTurn;
           }
         }
-        this.setState({boardData: updatedBoard})
-        
+        this.setState({ boardData: updatedBoard });
       }
 
       if (data.header === "check-win-from-server") {
-        console.log("WINNER: "+  data.winner);
-        if (data.winner === this.state.id ){
-          alert("YOU WIN !");
-        }else{
-          alert("YOU LOSE ...");
+        console.log("WINNER: " + data.winner);
+
+        if (data.winner === this.state.id) {
+          this.setState({ win: true, modalShow: true });
+        } else {
+          this.setState({ lose: true, modalLoseShow: true });
         }
       }
     });
@@ -108,12 +142,14 @@ export default class Board extends React.Component {
     this.setState({ nameOK: true });
   }
 
-
   componentDidMount() {
     const {
       match: { params }
     } = this.props;
     this.setState({ room: params.room });
+    // if (this.state.win){
+    //   this.setState({ modalShow: true });
+    // }
   }
 
   // Gets initial board data
@@ -160,18 +196,17 @@ export default class Board extends React.Component {
     return <div>{arr}</div>;
   }
 
-  getPlayerName(){
-    if (this.state.response){
-      return this.state.holdingX ? "❌" : "⭕"
+  getPlayerName() {
+    if (this.state.response) {
+      return this.state.holdingX ? "❌" : "⭕";
     }
   }
-  getOpp(){
-    if (this.state.response){
-      return !this.state.holdingX ? "❌" : "⭕"
+  getOpp() {
+    if (this.state.response) {
+      return !this.state.holdingX ? "❌" : "⭕";
     }
   }
-  
-  
+
   render() {
     let inputName = (
       <div>
@@ -191,13 +226,18 @@ export default class Board extends React.Component {
     let labelName = (
       <div className="vs">
         <label>
-          <h4>Player: {this.state.playerName}: {this.getPlayerName()}</h4>
+          <h4>
+            Player: {this.state.playerName}: {this.getPlayerName()}
+          </h4>
         </label>
         <label>
           <h2>vs</h2>
         </label>
         <label>
-          <h4> {this.state.opponent}: {this.getOpp()}</h4>
+          <h4>
+            {" "}
+            {this.state.opponent}: {this.getOpp()}
+          </h4>
         </label>
       </div>
     );
@@ -205,7 +245,7 @@ export default class Board extends React.Component {
     // this.setState({boardData: boardData});
 
     let board = (
-      <div className="board" id="boardplay" >
+      <div className="board" id="boardplay">
         {this.renderBoard(this.state.boardData)}
       </div>
     );
@@ -220,13 +260,66 @@ export default class Board extends React.Component {
     let turningBar = (
       <div className="line-sm">
         <div>Your turn ...</div>
-        <ProgressBar animated now={100} variant="info"/>
+        <ProgressBar animated now={100} variant="info" />
       </div>
-    )
+    );
 
+    let returnBtn = (
+      <Link to="/">
+        <Button className="room-btn" size="lg" variant="outline-primary" block>
+          Return Home
+        </Button>
+      </Link>
+    );
+    let reactionBar = (
+      <div className="line">
+        <Button variant="outline-light" size="lg" onClick={this.handleEmoji}>
+          <span role="img" aria-label="emoji" ref="emo">
+            😊
+          </span>
+        </Button>
+        <Button variant="outline-light" size="lg" onClick={this.handleEmoji}>
+          <span role="img" aria-label="emoji" ref="emo">
+            👍
+          </span>
+        </Button>
+        <Button variant="outline-light" size="lg" onClick={this.handleEmoji}>
+          <span role="img" aria-label="emoji" ref="emo">
+            👎
+          </span>
+        </Button>
+        <Button variant="outline-light" size="lg" onClick={this.handleEmoji}>
+          <span role="img" aria-label="emoji" ref="emo">
+            🤔
+          </span>
+        </Button>
+        <Button variant="outline-light" size="lg" onClick={this.handleEmoji}>
+          <span role="img" aria-label="emoji" ref="emo">
+            😢
+          </span>
+        </Button>
+      </div>
+    );
+    let modalClose = () =>
+      this.setState({
+        modalShow: false,
+        modalLoseShow: false,
+        modalEmoShow: false
+      });
     return (
       <SocketContext.Provider value={socket}>
-        <div className="game-board">
+        <ModalWinner
+          show={this.state.modalShow}
+          onHide={modalClose}
+          opponent={this.state.opponent}
+        />
+        <ModalLoser
+          show={this.state.modalLoseShow}
+          onHide={modalClose}
+          opponent={this.state.opponent}
+        />
+        <ModalEmoji show={this.state.modalEmoShow} onHide={modalClose} emoji={this.state.emoji} />
+        <div className="game-board" onClick={this.allowClick}>
           <Button variant="light" className="room-label">
             <Badge variant="light">
               <h2> {this.state.room}</h2>{" "}
@@ -243,12 +336,75 @@ export default class Board extends React.Component {
                 READY!
               </Button>
             )}
-            {this.state.isMyTurn && this.state.response ? turningBar : null}
+            {this.state.response ? reactionBar : null}
+            {this.state.isMyTurn &&
+            this.state.response &&
+            !this.state.win &&
+            !this.state.lose
+              ? turningBar
+              : null}
+            {this.state.win || this.state.lose ? returnBtn : null}
             {this.state.response ? board : null}
             {this.state.ready && !this.state.response ? waitBar : null}
           </div>
         </div>
       </SocketContext.Provider>
+    );
+  }
+}
+class ModalWinner extends React.Component {
+  render() {
+    return (
+      <Modal {...this.props} size="lg" aria-labelledby="modal-title" centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="modal-title">Congratulation!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h3>You beat the player "{this.props.opponent} "</h3>
+          <p>Click OK to Return to home</p>
+        </Modal.Body>
+        <Modal.Footer>
+          {/* <Button onClick={this.returnHome}>OK</Button> */}
+          <Link to="/">
+            <Button size="lg" variant="outline-info" block>
+              OK
+            </Button>
+          </Link>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+class ModalLoser extends React.Component {
+  render() {
+    return (
+      <Modal {...this.props} size="lg" aria-labelledby="modal-title" centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="modal-title">You're Defeated ...</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h3>You lost to the player "{this.props.opponent} "</h3>
+          <p>Click OK to Return to home</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Link to="/">
+            <Button size="lg" variant="outline-info" block>
+              OK
+            </Button>
+          </Link>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+class ModalEmoji extends React.Component {
+  render() {
+    return (
+      <Modal {...this.props} size="sm" aria-labelledby="modal-title" centered>
+        <Modal.Body>
+          <h1>{this.props.emoji}</h1>
+        </Modal.Body>
+      </Modal>
     );
   }
 }
--- src/component/RoomPicker.js
+++ src/component/RoomPicker.js
@@ -3,33 +3,45 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import "../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 class RoomPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [],
+      NumberRoom: 0
+    };
+    this.EnableRoom = this.EnableRoom.bind(this);
+  }
+  EnableRoom() {
+    console.log(this.state.rows);
+    let number = this.state.NumberRoom;
+    let link = "/play/room" + number;
+    let tempRows = this.state.rows;
+    tempRows.push(
+      <Link to={link} key={number}>
+        <Button className="room-btn" size="lg" variant="outline-info" block>
+          Room#{number < 10 ? "0" + number : number}
+        </Button>
+      </Link>
+    );
+    this.setState({ rows: tempRows });
+    this.setState({ NumberRoom: number + 1 });
+  }
   render() {
-    let rows = [];
-    for (let i = 0; i < 100; i++) {
-      let link = "/play/room"+i;
-      rows.push(
-        <Link to={link} key={i}>
-          <Button className="room-btn" size="lg" variant="outline-info" block>
-            Room#{i<10?'0'+i:i}
-          </Button>
-        </Link>
-      );
-    }
-
     return (
       <div>
         <div className="line">
-          <Button variant="outline-primary" className="create-btn">
+          <Button
+            variant="outline-primary"
+            className="create-btn"
+            onClick={this.EnableRoom}
+          >
             Create Room
           </Button>
         </div>
-        <div className="wrapper-room-btn">
-          {rows}
-
-        </div>
+        <div className="wrapper-room-btn">{this.state.rows}</div>
       </div>
     );
   }
